5524724b6b11ae2cc097b3af57209a78cd93c705
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-05-26 05:06:17 +0000

    Update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,7 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    game = @game
     @user = @game.user
     @comment = Comment.new
     @comments = @game.comments
