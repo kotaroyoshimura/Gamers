a0de4399bc27e38514325b26af0496f1b4365349
吉村侯太朗 <youwatanabeoku@yahoo.co.jp>
2021-06-25 19:53:41 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   scope module: :users do
     root to:'homes#top'
     get '/search' => 'search#search'
